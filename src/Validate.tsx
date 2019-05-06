87ced0337b9d2769346efaf6295180719cd4360b
na0711 <yagura.fes@gmail.com>
2019-05-06 11:50:21 +0900

    feat:不具合修正

--- src/Validate.tsx
+++ src/Validate.tsx
@@ -2,16 +2,63 @@ import React, { useState } from "react";
 import "./Validate.scss";
 type Val = string | number;
 
+type IValidate = {
+  [key: string]: {
+    value: string;
+    err: string;
+  };
+};
 type InputProps = {
   name: string;
-  type: string;
-  placeholder: string;
+  type: "text" | "tel" | "url" | "email" | "password" | "hidden";
   value?: Val;
   rule?: string;
-  err?: string;
+  placeholder: string;
+  saveState: React.Dispatch<React.SetStateAction<IValidate>>;
+  state: IValidate;
+};
+type RadioProps = {
+  name: string;
+  type: "radio";
+  value?: Val;
+  rule?: string;
+  items: {
+    value: Val;
+    label: Val;
+  }[];
+  saveState: React.Dispatch<React.SetStateAction<IValidate>>;
+  state: IValidate;
+};
+type CheckboxProps = {
+  name: string;
+  type: "checkbox";
+  value?: Val;
+  rule?: string;
+  items: {
+    value: Val;
+    label: Val;
+  }[];
+  saveState: React.Dispatch<React.SetStateAction<IValidate>>;
+  state: IValidate;
+};
+type SelectProps = {
+  name: string;
+  value?: Val;
+  rule?: string;
+  items: {
+    value: Val;
+    label: Val;
+  }[];
+  saveState: React.Dispatch<React.SetStateAction<IValidate>>;
+  state: IValidate;
+};
+type IconProps = {
+  label: string;
+  className: string;
 };
 type Rule = {
   require: boolean | Function;
+  type?: Array<string | { [key: string]: number }>;
   msg: {
     [key: string]: string;
   };
@@ -20,125 +67,367 @@ type Rule = {
 const allRules: {
   [key: string]: Rule;
 } = {
-  nameKanji: {
+  namae: {
+    require: true,
+    type: [{ maxLength: 20 }],
+    msg: {
+      require: "必須項目です。",
+      maxLength: "20文字以内で入力してください"
+    }
+  },
+  hurigana: {
+    require: false,
+    type: ["kana", { maxLength: 20 }],
+    msg: {
+      kana: "カタカナで入力してください。",
+      maxLength: "20文字以内で入力してください"
+    }
+  },
+  pref: {
     require: true,
     msg: {
       require: "必須項目です。"
     }
+  },
+  mail: {
+    require: false,
+    type: ["email", { maxLength: 20 }],
+    msg: {
+      require: "必須項目です。",
+      maxLength: "20文字以内で入力してください",
+      email: "メールアドレスの形式で入力してください。"
+    }
   }
 };
 
-const validate = (val: Val, rule: Rule) => {
+const validate = (val: Val, rule: Rule): string => {
   let requireFlg = false;
+  // 必須チェック
   if (typeof rule.require === "function") {
     requireFlg = rule.require();
   } else {
     requireFlg = rule.require;
   }
   if (requireFlg && val === "") {
-    return rule.msg.require;
+    return "require";
+  }
+  // typeチェック
+  if (!rule.type) {
+    return "";
+  }
+  for (let i = 0; i < rule.type.length; i++) {
+    const type = rule.type[i];
+    // メアド型
+    if (val !== "" && type === "email") {
+      if (!String(val).match(/^[^@]+@.+\..+$/)) {
+        return type;
+      }
+    }
+    // カタカナ型
+    if (val !== "" && type === "kana") {
+      if (!String(val).match(/^[ァ-ヶー　]*$/)) {
+        return type;
+      }
+    }
+    // 最大値/最小値/文字数チェック
+    if (typeof type === "object") {
+      const typeNameArr = Object.keys(type);
+      for (let j = 0; j < typeNameArr.length; j++) {
+        const typeName = typeNameArr[j];
+        if (typeName === "maxLength") {
+          if (type[typeName] < String(val).length) {
+            return "maxLength";
+          }
+        }
+        if (typeName === "minValue") {
+          if (type[typeName] > Number(val)) {
+            return "minValue";
+          }
+        }
+        if (typeName === "miaxValue") {
+          if (type[typeName] < Number(val)) {
+            return "miaxValue";
+          }
+        }
+      }
+    }
   }
   return "";
 };
 
-const Input: React.FC<InputProps> = props => {
-  const [state, saveState] = useState({
-    value: props.value,
-    err: ""
-  });
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const val = event.currentTarget.value;
-    let errMsg = "";
-    // ルール指定やルール定義がなければエラーをクリア
-    if (!props.rule || !allRules[props.rule]) {
-      errMsg = "";
-    } else {
-      const rule = allRules[props.rule];
-      errMsg = validate(val, rule);
-    }
-    saveState({ ...state, value: val, err: errMsg });
-  };
+const Input: React.FC<RadioProps | CheckboxProps | InputProps> = props => {
+  if (props.type === "radio") {
+    return <InputRadio {...props} />;
+  } else if (props.type === "checkbox") {
+    return <InputCheckbox {...props} />;
+  } else {
+    return <InputText {...props} />;
+  }
+};
+
+const InputText: React.FC<InputProps> = props => {
   return (
     <div className="form__wrap">
       <input
         type={props.type}
         name={props.name}
-        value={state.value}
+        value={props.state[props.name]!.value}
         placeholder={props.placeholder}
-        onChange={handleChange}
+        onChange={e => handleChange(e, props)}
         className="form__input"
       />
-      <div className={`form__error ${state.err ? "is-error" : ""}`}>
-        {state.err}
+      <div
+        className={`form__error ${
+          props.state[props.name].err ? "is-error" : ""
+        }`}
+      >
+        {props.state[props.name].err}
       </div>
     </div>
   );
 };
-
-const Radio: React.FC<RadioProps> = props => {
-  const [state, saveState] = useState({
-    value: props.value,
-    err: ""
-  });
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const val = event.currentTarget.value;
-    let errMsg = "";
-    // ルール指定やルール定義がなければエラーをクリア
-    if (!props.rule || !allRules[props.rule]) {
-      errMsg = "";
-    } else {
-      const rule = allRules[props.rule];
-      errMsg = validate(val, rule);
-    }
-    saveState({ ...state, value: val, err: errMsg });
-  };
+const InputRadio: React.FC<RadioProps> = props => {
+  return (
+    <div className="">
+      {props.items.map(item => {
+        return (
+          <label className="form__wrap" key={item.label}>
+            <input
+              type={props.type}
+              name={props.name}
+              value={item.value}
+              onChange={e => handleChange(e, props)}
+              className="form__radio"
+            />
+            <div className="form__label">{item.label}</div>
+          </label>
+        );
+      })}
+      <div
+        className={`form__error ${
+          props.state[props.name].err ? "is-error" : ""
+        }`}
+      >
+        {props.state[props.name].err}
+      </div>
+    </div>
+  );
+};
+const InputCheckbox: React.FC<CheckboxProps> = props => {
+  return (
+    <div className="">
+      {props.items.map(item => {
+        return (
+          <label className="form__wrap" key={item.label}>
+            <input
+              type={props.type}
+              name={props.name}
+              value={item.value}
+              onChange={e => handleChange(e, props)}
+              className="form__checkbox"
+            />
+            <div className="form__label">{item.label}</div>
+          </label>
+        );
+      })}
+      <div
+        className={`form__error ${
+          props.state[props.name].err ? "is-error" : ""
+        }`}
+      >
+        {props.state[props.name].err}
+      </div>
+    </div>
+  );
+};
+const Select: React.FC<SelectProps> = props => {
   return (
     <div className="form__wrap">
-      <input
-        type={props.type}
-        name={props.name}
-        value={state.value}
-        placeholder={props.placeholder}
-        onChange={handleChange}
-        className="form__input"
-      />
-      <div className={`form__error ${state.err ? "is-error" : ""}`}>
-        {state.err}
+      <label className="form__select-wrap">
+        <select
+          name={props.name}
+          value={props.state[props.name].value}
+          onChange={e => handleChange(e, props)}
+          className="form__select"
+        >
+          {props.items.map(item => {
+            return (
+              <option value={item.value} key={item.label}>
+                {item.label}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+      <div
+        className={`form__error ${
+          props.state[props.name].err ? "is-error" : ""
+        }`}
+      >
+        {props.state[props.name].err}
       </div>
     </div>
   );
 };
-
-const Validate: React.FC = props => {
-  const sexRadio = [
-    {
-      val: 0,
-      l: "男性"
-    },
-    {
-      val: 0,
-      lalen: "女性"
+const Icon = (props: IconProps) => {
+  return <span className={props.className}>{props.label}</span>;
+};
+const handleChange = (
+  event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  props: React.PropsWithChildren<
+    InputProps | RadioProps | CheckboxProps | SelectProps
+  >
+) => {
+  const val = event.currentTarget.value;
+  let errMsg = "";
+  // ルール定義がある場合のみ実行
+  if (allRules[props.name]) {
+    const rule = allRules[props.name];
+    const result: string = validate(val, rule);
+    if (result) {
+      errMsg = rule.msg[result];
     }
-  ];
+  }
+  const newState = { ...props.state };
+  newState[props.name] = {
+    value: val,
+    err: errMsg
+  };
+  props.saveState(newState);
+};
+const handleClick = (
+  state: IValidate,
+  saveState: React.Dispatch<React.SetStateAction<IValidate>>
+) => {
+  const nameArr = Object.keys(state);
+  const newState = { ...state };
+  nameArr.forEach(name => {
+    const val = state[name].value;
+    const rule = allRules[name];
+    if (rule) {
+      const result: string = validate(val, rule);
+      if (result) {
+        newState[name]["err"] = rule.msg[result];
+      }
+    }
+  });
+  saveState(newState);
+};
+const Validate: React.FC = () => {
+  const [state, saveState] = useState<IValidate>({
+    namae: { value: "", err: "" },
+    hurigana: { value: "", err: "" },
+    sex: { value: "", err: "" },
+    pref: { value: "", err: "" },
+    mail: { value: "", err: "" },
+    mailmag: { value: "", err: "" }
+  });
+  const errArr: string[] = Object.values(state).map(item => {
+    return item.err;
+  });
+  const errFlg = errArr.some(err => {
+    return err !== "";
+  });
   return (
     <form name="hoge" method="POST" action="./confirm/" className="">
-      <Input
-        name="name"
-        type="text"
-        value="山田太郎"
-        placeholder="必須：お名前"
-        rule="nameKanji"
-      />
-      <Input
-        name="kana"
-        type="text"
-        value="ヤマダタロウ"
-        placeholder="フリガナ"
-        rule="nameKana"
-      />
-      <Radio name="sex" type="radio" value={} rule="sex" />
+      <table className="table">
+        <tbody>
+          <tr>
+            <th className="table__th">
+              お名前
+              <Icon className="icon--require" label="必須" />
+            </th>
+            <td className="table__td">
+              <Input
+                name="namae"
+                type="text"
+                value=""
+                placeholder="お名前"
+                state={state}
+                saveState={saveState}
+              />
+            </td>
+          </tr>
+          <tr>
+            <th className="table__th">フリガナ</th>
+            <td className="table__td">
+              <Input
+                name="hurigana"
+                type="text"
+                value=""
+                placeholder="フリガナ"
+                state={state}
+                saveState={saveState}
+              />
+            </td>
+          </tr>
+          <tr>
+            <th className="table__th">性別</th>
+            <td className="table__td">
+              <Input
+                name="sex"
+                type="radio"
+                value=""
+                items={[
+                  { value: 0, label: "男性" },
+                  { value: 1, label: "女性" }
+                ]}
+                state={state}
+                saveState={saveState}
+              />
+            </td>
+          </tr>
+          <tr>
+            <th className="table__th">
+              都道府県
+              <Icon className="icon--require" label="必須" />
+            </th>
+            <td className="table__td">
+              <Select
+                name="pref"
+                value=""
+                items={[
+                  { value: "", label: "選択してください" },
+                  { value: 1, label: "北海道" }
+                ]}
+                state={state}
+                saveState={saveState}
+              />
+            </td>
+          </tr>
+          <tr>
+            <th className="table__th">メールアドレス</th>
+            <td className="table__td">
+              <Input
+                name="mail"
+                type="email"
+                value=""
+                placeholder="example@example.com"
+                state={state}
+                saveState={saveState}
+              />
+              <Input
+                name="mailmag"
+                type="checkbox"
+                value=""
+                items={[{ value: 1, label: "メールマガジンを受け取る" }]}
+                state={state}
+                saveState={saveState}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="btn__wrap">
+        <button
+          className="btn--submit"
+          onClick={() => handleClick(state, saveState)}
+          disabled={errFlg}
+        >
+          送信
+        </button>
+      </div>
     </form>
   );
 };
-
 export default Validate;
